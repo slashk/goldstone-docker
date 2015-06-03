89a39fb5a831e67f85f2c7f2b7fa2249f6fc5f87
John Stanford <john@solinea.com>
2015-06-03 14:45:52 -0700

    renamed logstash, added plugin to es, added goldstone init to postgres

---
+++ Dockerfiles/goldstone-postgres/init-goldstone-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pass="'$GOLDSTONE_PASSWORD'"
+gosu postgres postgres --single -E <<- EOF
+    CREATE USER goldstone PASSWORD $pass;
+    CREATE DATABASE goldstone_docker;
+    GRANT ALL PRIVILEGES ON DATABASE goldstone_docker TO goldstone;
+EOF
+
