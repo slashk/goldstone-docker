d8ba338984a39ddc1f9aaf5aa600e66783bc23ae
John Stanford <john@solinea.com>
2015-06-05 10:50:09 -0700

    added createdb permissions for goldstone user

--- Dockerfiles/goldstone-postgres/init-goldstone-db.sh
+++ Dockerfiles/goldstone-postgres/init-goldstone-db.sh
@@ -5,5 +5,6 @@ gosu postgres postgres --single -E <<- EOF
     CREATE USER goldstone PASSWORD $pass;
     CREATE DATABASE goldstone_docker;
     GRANT ALL PRIVILEGES ON DATABASE goldstone_docker TO goldstone;
+    ALTER USER goldstone CREATEDB;
 EOF
 
